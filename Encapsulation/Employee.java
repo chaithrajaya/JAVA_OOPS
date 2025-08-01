174e558fbf9fb9dde6bbdfdc0461c45afa42f27d
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-01 21:45:09 +0530

    Update Employee.java

--- Encapsulation/Employee.java
+++ Encapsulation/Employee.java
@@ -1,1 +1,34 @@
+class Employee{
+    private String name;
+    private double salary;
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setSalary(Double newSalary){
+        if(newSalary>0){
+       salary = newSalary;
+        }
+        else{
+            System.out.println("Invalid Salary");
+        }
+        
+    }
+    public String getName(){
+        return name;
+    }
+    public double getSalary(){
+        return salary;
+    }
 
+
+}
+
+public class Main{
+    public static void main(String[] args){
+        Employee e = new Employee();
+        e.setName("ABC");
+        e.setSalary(60000.0);
+        System.out.println("Employee Name: "+ e.getName());
+        System.out.println("Employee Salary: "+ e.getSalary());
+    }
+}
