e6d434743f5d463c7a147582707c9e464913c1f6
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-01 12:42:05 +0530

    Update AreaCalculator.java

--- Method Overloading/AreaCalculator.java
+++ Method Overloading/AreaCalculator.java
@@ -10,8 +10,8 @@ class AreaCalculator{
     }
     public static void main(String[] args){
         AreaCalculator calc = new AreaCalculator();
-        System.out.println("Area of a square is: "+ calc.area_square(10));
-         System.out.println("Area of a rectangle is: "+ calc.area_rectangle(10,20));
-          System.out.println("Area of a circle is: "+ calc.area_circle(10));
+        System.out.println("Area of a square is: "+ calc.area(10));
+         System.out.println("Area of a rectangle is: "+ calc.area(10,20));
+          System.out.println("Area of a circle is: "+ calc.area(10));
     }
 }
