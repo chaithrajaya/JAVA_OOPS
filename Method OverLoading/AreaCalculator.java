e52d5af0ab9431fda6534df3876e76f52c2da3c2
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-01 20:20:45 +0530

    Add files via upload

--- Method OverLoading/AreaCalculator.java
+++ Method OverLoading/AreaCalculator.java
@@ -1,1 +1,18 @@
-
+class AreaCalculator{
+    int area(int side){
+        return side*side;
+    }
+    
+    int area(int length,int breadth){
+        return length*breadth;
+    }
+    double area(double radius){
+        return 3.14*radius*radius;
+    }
+    public static void main(String[] args){
+        AreaCalculator calc = new AreaCalculator();
+        System.out.println("Area of a square is: "+ calc.area(10));
+         System.out.println("Area of a rectangle is: "+ calc.area(10,20));
+          System.out.println("Area of a circle is: "+ calc.area(10));
+    }
+}
