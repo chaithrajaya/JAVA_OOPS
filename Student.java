707120208bcc94d16f5e197562168adf35eb4128
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-01 07:34:16 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+class Student{
+   int age;
+        String name;
+
+Student(String n,int a){
+    name = n;
+    age = a;
+}
+void display(){
+    System.out.println("Name is: " + name);
+    System.out.println("Age is: " + age);
+}
+ public static void main(String[] args){
+        
+        Student s1 = new Student("ABC", 12);
+        s1.display();
+    }
+}
+
+//output:
+//        Name is: ABC  
+//        Age is: 12
