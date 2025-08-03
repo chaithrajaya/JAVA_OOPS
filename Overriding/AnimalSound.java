e10ee46c47a6210b02604826d4a536c1dbf1b1ea
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-03 20:10:47 +0530

    Add files via upload

---
+++ Overriding/AnimalSound.java
@@ -0,0 +1,17 @@
+class Animal{
+    void makeSound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal{
+    @Override
+    void makeSound(){
+        System.out.println("Dog barks");
+    }
+}
+public class AnimalSound{
+    public static void main(String[] args){
+        Animal a = new Dog();
+        a.makeSound();
+    }
+}
