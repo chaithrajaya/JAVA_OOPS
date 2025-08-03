60540b5e3314ca70b077ab9e128f2406ae531d55
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-03 20:04:32 +0530

    Add files via upload

---
+++ Overriding/Main1.java
@@ -0,0 +1,18 @@
+class Shape{
+    void draw(){
+        System.out.println("Drawing shape");
+    }
+}
+class Circle extends Shape{
+    @Override
+    void draw(){
+        System.out.println("Drawing circle");
+    }
+
+}
+public class Main1{
+public static void main(String[] args){
+    Shape s = new Circle();
+    s.draw();
+}
+}
