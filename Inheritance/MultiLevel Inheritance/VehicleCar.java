db2427f44d0cc4ec1cd12e8e8408106d89747427
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-04 20:55:55 +0530

    Add files via upload

--- Inheritance/MultiLevel Inheritance/VehicleCar.java
+++ Inheritance/MultiLevel Inheritance/VehicleCar.java
@@ -1,1 +1,30 @@
+class Vehicle{
+    void start(){
+        System.out.println("Vehicle is starting");
+    }
+}
+class Car extends Vehicle{
+    void drive(){
+        System.out.println("Car is being driven");
+    }
+}
+class ElectricCar extends Car{
+    void charge(){
+        System.out.println("Electric car is charging");
+    }
+}
 
+public class VehicleCar{
+    public static void main(String[] args){
+        ElectricCar E = new ElectricCar();
+        E.start();
+        E.drive();
+        E.charge();
+    }
+}
+
+/*
+Vehicle is starting
+Car is being driven
+Electric car is charging
+*/
