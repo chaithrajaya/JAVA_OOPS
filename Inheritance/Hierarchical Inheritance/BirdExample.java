1dae8571235e9fc4b0900713ea94221cbf565367
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-04 21:17:24 +0530

    Add files via upload

--- Inheritance/Hierarchical Inheritance/BirdExample.java
+++ Inheritance/Hierarchical Inheritance/BirdExample.java
@@ -1,1 +1,33 @@
+class Bird{
+    void fly(){
+        System.out.println("Bird is flying");
+    }
+}
+class Sparrow extends Bird{
+    void chirp(){
+        System.out.println("Sparrow is chirping");
+    }
+}
+class Eagle extends Bird{
+    void hunt(){
+        System.out.println("Eagle is hunting");
+    }
+}
+public class BirdExample{
+    public static void main(String[] args){
+        Sparrow S = new Sparrow();
+        S.fly();
+        S.chirp();
 
+        Eagle E = new Eagle();
+        E.fly();
+        E.hunt();
+    }
+}
+
+/*
+Bird is flying  
+Sparrow is chirping  
+Bird is flying  
+Eagle is hunting
+*/
