552019e4167a78fa2610ed6304b8cf8db2bce79b
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-04 20:26:35 +0530

    Add files via upload

--- Inheritance/Single Inheritance/PersonStudent.java
+++ Inheritance/Single Inheritance/PersonStudent.java
@@ -1,1 +1,20 @@
+class Person{
+    void introduce(){
+        System.out.println("I am a person");
+    }
+}
+class Student extends Person{
+    void study(){
+        System.out.println("I am studying");
+    }
+}
+public class PersonStudent{
+    public static void main(String[] args){
+        Student s = new Student();
+        s.introduce();
+        s.study();
+    }
+}
 
+//I am a person
+//I am studying
