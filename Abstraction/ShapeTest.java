f6510097c159bb8d4ba5322d4d804996d5f01b07
CHAITHRA S <147913846+chaithrajaya@users.noreply.github.com>
2025-08-05 20:43:37 +0530

    Add files via upload

--- Abstraction/ShapeTest.java
+++ Abstraction/ShapeTest.java
@@ -1,1 +1,46 @@
+abstract class Shape{
+    abstract void calculateArea();
+    void display(){
+        System.out.println("This is a shape");
+    }
+}
+class Circle extends Shape{
 
+    double radius;
+    Circle(double radius){
+        this.radius = radius;
+    }
+
+    void calculateArea(){
+        double area = Math.PI*radius*radius;
+
+        System.out.println("Area of Circle: "+ area);
+    }
+}
+class Rectangle extends Shape{
+    double length,breadth;
+
+    Rectangle (double length,double breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+    void calculateArea(){
+        double area = length*breadth;
+        System.out.println("Area of Rectangle: "+area);
+    }
+}
+
+public class ShapeTest{
+    public static void main(String[] args){
+        Circle c = new Circle(5);
+        Rectangle r = new Rectangle(4,6);
+
+        System.out.println("Circle");
+        c.display();
+        c.calculateArea();
+
+        System.out.println("\nRectangle: ");
+        r.display();
+        r.calculateArea();
+    }
+}
